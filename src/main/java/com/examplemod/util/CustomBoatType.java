6772673579a84c846eb90f9e1b3ae7edfabe488b
nyuppo <46286273+nyuppo@users.noreply.github.com>
2021-12-10 10:13:00 -0500

    Create CustomBoatType.java

---
+++ src/main/java/com/examplemod/util/CustomBoatType.java
@@ -0,0 +1,9 @@
+import net.minecraft.entity.vehicle.BoatEntity;
+
+public class CustomBoatType {
+    static {
+        BoatEntity.Type.values();
+    }
+
+    public static BoatEntity.Type WILLOW;
+}
